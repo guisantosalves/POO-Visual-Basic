3709c4748656511976e721065d9690310b7a7c1a
guisantosalves <workguisantos@gmail.com>
2022-07-12 22:38:33 -0400

    poo in visual basic with exemple

---
+++ POO_VB/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim retangulo1 As New Retangulo(15, 20)
+
+        Console.WriteLine("sua área é: {0}", retangulo1.GetArea().ToString)
+
+
+
+    End Sub
+End Module
---
+++ POO_VB/Retangulo.vb
@@ -0,0 +1,31 @@
+﻿Public Class Retangulo
+    'meus atributos
+    Private _base As Double
+    Private _altura As Double
+
+    'construtor
+    'pode ter vários construtores mas com parâmetros diferentes
+    Public Sub New(base As Double, altura As Double)
+        _base = base
+        _altura = altura
+    End Sub
+
+    Public Sub New()
+
+    End Sub
+
+    'function sempre retornam
+    Public Function GetArea() As Double
+        If _base > 0 And _altura > 0 Then
+            Return _base * _altura
+        Else
+            Return -1
+        End If
+    End Function
+
+    'sub nao retornam nada
+    Public Sub initFields(base As Double, altura As Double)
+        _base = base
+        _altura = altura
+    End Sub
+End Class
